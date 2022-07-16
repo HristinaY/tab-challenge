df63c45c7688a7001bfffd7caec35ae4524b3e29
HristinaY <hristina.yovcheva@gmail.com>
2022-07-16 17:32:28 +0100

    js, active stage css

--- tabs.js
+++ tabs.js
@@ -1,1 +1,14 @@
-console.log('log from tabs.js');
+const tabsCotainer = document.getElementById("tabs");
+
+tabsCotainer.addEventListener("click", (event) => {
+  let activeTab = event.target;
+  activeTab.classList.contains("tab-title")
+    ? (activeTab = event.target)
+    : activeTab.closest(".tab-title");
+
+  const tabs = document.querySelectorAll(".tab-title");
+  tabs.forEach((tab) => {
+    tab.classList.remove("active");
+  });
+  activeTab.classList.toggle("active");
+});
